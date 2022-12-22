572a2204b86c8bfa35eb889639ad0db18fa43b5b
denis <106389581+fishman123456@users.noreply.github.com>
2022-12-22 17:29:56 +0700

    Initial commit

---
+++ Export_DataGridView_HTML_CS/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Export_DataGridView_HTML_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id", typeof(int)),
+                                   new DataColumn("Name", typeof(string)),
+                                   new DataColumn("Country",typeof(string)) });
+            dt.Rows.Add(1, "John Hammond", "United States");
+            dt.Rows.Add(2, "Mudassar Khan", "India");
+            dt.Rows.Add(3, "Suzanne Mathews", "France");
+            dt.Rows.Add(4, "Robert Schidner", "Russia");
+            this.dataGridView1.DataSource = dt;
+            this.dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            //Table start.
+            string html = "<table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 9pt;font-family:arial'>";
+
+            //Adding HeaderRow.
+            html += "<tr>";
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                html += "<th style='background-color: #B8DBFD;border: 1px solid #ccc'>" + column.HeaderText + "</th>";
+            }
+            html += "</tr>";
+
+            //Adding DataRow.
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                html += "<tr>";
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    html += "<td style='width:120px;border: 1px solid #ccc'>" + cell.Value.ToString() + "</td>";
+                }
+                html += "</tr>";
+            }
+
+            //Table end.
+            html += "</table>";
+
+            File.WriteAllText(@"first.html", html); 
+        }
+    }
+}
